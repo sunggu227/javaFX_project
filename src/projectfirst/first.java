cf9e9831a15329384c6b95e3bd4bfb39d7f9d078
csh <abdem5@naver.com>
2020-06-25 16:12:42 +0900

    처음올리는

--- src/projectfirst/first.java
+++ src/projectfirst/first.java
@@ -2,6 +2,7 @@ package projectfirst;
 
 public class first {
 	public static void main(String[] args) {
-		System.out.println("처음 출력 ");
+		System.out.println("처음출력  ");
+		System.out.println("왤케어려움 ");
 	}
 }
